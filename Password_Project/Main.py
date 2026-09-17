82099a6503d4093e9464470b5ea0c93d06247bf5
LeviDev <134110859+LeviDev69@users.noreply.github.com>
2026-09-17 17:43:42 +0000

    Added intro parts

--- Password_Project/Main.py
+++ Password_Project/Main.py
@@ -4,22 +4,29 @@ def tprint(text):
   for character in text:
     sys.stdout.write(character)
     sys.stdout.flush()
-    time.sleep(0.05)
+    time.sleep(0.04)
     
 quit_statements = ["q", "quit", "exit", "exit game"]
 
 def qinput(prompt):
-  u = input(prompt)
+  tprint(prompt)
+  u = input()
   if u in quit_statements:
     sys.exit("User Exited")
   return u
+
+def innit():
+  user = qinput("please input your name: ")
+  tprint("Welcome to Project J.A.C.K. GPT If you want to quit at any time input q\n")
+  tprint("======================================================================================================\n")
+  tprint(f"Name: {user} \nCareer:  D.A.F.D.A.L.U.B (Dumpsters Association for Dumb Adults Like the Uttlery Broke)\n")
+  tprint("Vehicle: Magic carpet\nAdress: Random Burger King Parking lot\n")
 
 def game():
   score = 0
   shield_points = 0
   sheild_health = 0
 
-  user = input("please input the main charater's name: ")
 
   pas = input("please enter a password:\n")
   score = 0  
@@ -31,8 +38,6 @@ def game():
      score += 1
   if any(char.isdigit() for char in pas):
      score += 1
-
-  print(score)
  
   if score == 1:
     shield_points = 2
@@ -46,12 +51,8 @@ def game():
   else:
     shield_points = 20 
     sheild_health += shield_points
-
-
-  if user != "admin":
-    admin = False
    
 if __name__ == "__main__":
+  innit()
   game()
-
 
