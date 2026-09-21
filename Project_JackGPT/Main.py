8e8d2b6272f2afd1b12a6213bca0c603fca49c1c
LeviDev <134110859+LeviDev69@users.noreply.github.com>
2026-09-21 14:48:36 +0000

    Seperated out project jackgpt from the password sheild builder project.

--- Project_JackGPT/Main.py
+++ Project_JackGPT/Main.py
@@ -15,43 +15,12 @@ def qinput(prompt):
     sys.exit("User Exited")
   return u
 
-def innit():
+def game():
   user = qinput("please input your name: ")
   tprint("Welcome to Project J.A.C.K. GPT If you want to quit at any time input q\n")
   tprint("====================================================================================================\n")
   tprint(f"Name: {user} \nCareer:  D.A.F.D.A.L.U.B (Dumpsters Association for Dumb Adults Like the Uttlery Broke)\n")
   tprint("Vehicle: Magic carpet\nAdress: Random Burger King Parking lot\n")
-
-def game():
-  score = 0
-  shield_points = 0
-  sheild_health = 0
-
-
-  pas = input("please enter a password:\n")
-  score = 0  
-  if len(pas) >= 8:
-     score += 1
-  if any(char.isupper() for char in pas):
-     score += 1
-  if any(char.islower() for char in pas):
-     score += 1
-  if any(char.isdigit() for char in pas):
-     score += 1
- 
-  if score == 1:
-    shield_points = 2
-   
-  elif score == 2:
-    shield_points = 5
-   
-  elif score == 3:
-    shield_points = 10
-   
-  else:
-    shield_points = 20 
-    sheild_health += shield_points
-
 
   tprint("Today while you were at work your co-worker, Jeffy, Climbed out of a dumpster covered in sludge. He had placed a milk jug on his back, and told you he was a snail. He asks you to throw salt on him and call him a bad boy\nOptions:\n1:Play Along\n2:Report him to HR\n3:Ignore him\n4:Play the Banjo in a summer breeze\n")
   try:
@@ -65,12 +34,13 @@ def game():
       tprint("You call HR to report him, as you raise your phone to your ear jeffy sees who you are calling and lunges at you, accidentally snaping your neck in the proccess\n")
       sys.exit("GAME OVER: You died")
     elif option == 3:
-      tprint("")
+      tprint("Jeffy snarls at you as you walk away")
+    else:
+      tprint("the music hypnotizes jeffy, he will now follow you. (+5 to all attack damage)\n")
   except Exception as e:
     print(f"ERROR: {e}")
 
 
 if __name__ == "__main__":
-  innit()
   game()
 
