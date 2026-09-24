796425b59e573aa7d83d09fbb95198afcf6cf46c
LeviDev <134110859+LeviDev69@users.noreply.github.com>
2026-09-24 16:22:44 +0000

    fixed tprint

--- Project_JackGPT/Main.py
+++ Project_JackGPT/Main.py
@@ -13,7 +13,7 @@ def tprint(text):
   for character in text:
     sys.stdout.write(character)
     sys.stdout.flush()
-    time.sleep(0.04)
+    time.sleep(0.07)
     
 quit_statements = ["q", "quit", "exit", "exit game"]
 
